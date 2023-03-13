0e078d0ea40106cc81c64c9a3a54c99bed84409e
qitianyb <942156930@qq.com>
2023-03-13 10:14:57 +0800

    qitian-dozer开始构建对象转换工具

---
+++ qitian-tools/qitian-dozer/src/test/java/com/example/qitiandozer/QitianDozerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qitiandozer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QitianDozerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
