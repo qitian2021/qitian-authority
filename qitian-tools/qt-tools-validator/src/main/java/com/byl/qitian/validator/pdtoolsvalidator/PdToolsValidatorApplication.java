8b6d7dd2e54a907f3dbac4f682c19a55393b29e2
qitianyb <942156930@qq.com>
2023-03-14 16:57:30 +0800

    qitian-dozer开始构建对象转换工具

---
+++ qitian-tools/qt-tools-validator/src/main/java/com/byl/qitian/validator/pdtoolsvalidator/PdToolsValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.byl.qitian.validator.pdtoolsvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdToolsValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdToolsValidatorApplication.class, args);
+    }
+
+}
---
+++ qitian-tools/qt-tools-validator/src/test/java/com/byl/qitian/validator/pdtoolsvalidator/PdToolsValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byl.qitian.validator.pdtoolsvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdToolsValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
